2e1d7fd65eb7091dd3f109d27b0a412846fa98b9
jackc7 <jack7curran@gmail.com>
2020-08-29 03:01:25 -0400

    Working

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 # 8/29/2020
 
-from readchar import readchar
-from colorama import Fore
-from time import time
 
+from time import time, sleep
+from colorama import Fore
+
+import readchar
 import random
 import sys
-
-
-paragraph = None
-text = None
 
 
 def end(starting_time):
     ending_time = time()
     time_diff = ending_time - starting_time
+
     return time_diff
+
 
 def random_paragraph():
     with open("sentences.txt", "r") as file:
         random_number = random.randint(0, 49)
         lines = file.readlines()
-        the_paragraph = lines[random_number][:-1]
-    return the_paragraph
+        random_paragraph = lines[random_number][:-1]
 
-def body(body):
-    global paragraph
-    paragraph = body
-    
-    global text
+    return random_paragraph
+
+
+def body(paragraph):
     text = []
 
     start = time()
@@ -37,7 +34,7 @@ def body(body):
         print('\033c' + Fore.GREEN + "".join(text) + Fore.WHITE + paragraph[i:])
 
         try:
-            button = readchar().decode("utf-8")
+            button = readchar.readchar().decode("utf-8")
             if button != paragraph[i]:
                 time_delta = end(start)
                 return time_delta, False
@@ -50,16 +47,26 @@ def body(body):
             time_delta = end(start)
             return time_delta, True
 
+
 def menu():
-    print(Fore.YELLOW + "Typing Speed Test: Press Enter to Begin" + Fore.WHITE)
-    input()
-    time_delta, is_complete = body()
+    paragraph = random_paragraph()
+    
+    print("\033c" + Fore.YELLOW + "Typing Speed Test: Press" + Fore.LIGHTYELLOW_EX + " any button " + Fore.YELLOW + "to begin." + Fore.WHITE)
+    readchar.readkey()
+
+    time_delta, is_complete = body(paragraph)
+
     if is_complete is False:
         wpm = len(paragraph) / 5 / time_delta * 60
-        print('\033c' + Fore.RED + "Incorrect." + Fore.GREEN + str(round(wpm)) + Fore.WHITE + "words per minute.")
+        print('\033c' + Fore.RED + "Incorrect. " + Fore.GREEN + str(round(wpm)) + Fore.WHITE + " words per minute.")
+        sleep(2)
+
     else:
         wpm = len(paragraph) / 5 / time_delta * 60
-        print(f"Sentence completed at {Fore.GREEN + str(round(wpm)) + Fore.WHITE} words per minute")
+        print("\033c" + f"Sentence completed at {Fore.GREEN + str(round(wpm)) + Fore.WHITE} words per minute")
+        sleep(2)
+
 
 if __name__ == "__main__":
-    menu()
+    while True:
+        menu()
